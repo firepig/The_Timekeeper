6e087cc492adec5f3cc5596e12050360eb926f93
firepig <korycthompson@gmail.com>
2025-03-19 18:08:20 -0400

    removed old config

--- config.js
+++
@@ -1,6 +0,0 @@
-// config.js (make sure to add this file to your .gitignore)
-const COUCHDB_USERNAME = "USERNAME";
-const COUCHDB_PASSWORD = "PASSWORD";
-const COUCHDB_HOST = "COUCHDB_SERVER_ADDRESS";
-const COUCHDB_PORT = "5984";
-const COUCHDB_DB = "my_database";
